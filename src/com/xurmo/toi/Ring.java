8605a3d0293224bd0a7f71d062a1c5514ebe21d6
Sreejith Sreekumar <sreejith2904@gmail.com>
2012-01-23 17:43:13 +0530

    All files re-commited

---
+++ src/com/xurmo/toi/Ring.java
@@ -0,0 +1,32 @@
+package com.xurmo.toi;
+
+/*
+ * @Author : Sreejith S
+ */
+
+public class Ring {
+
+	int iD;
+
+	public Ring(int iD) {
+		this.iD = iD;
+	}
+	
+	
+	public void move(Tower srcTower, Tower destTower){
+		if(srcTower.getRingOnTop().getiD() < destTower.getRingOnTop().getiD())
+			destTower.rings.add(srcTower.getRingOnTop());
+	}
+
+
+	public int getiD() {
+		return iD;
+	}
+
+
+	public void setiD(int iD) {
+		this.iD = iD;
+	}
+	
+	
+}
